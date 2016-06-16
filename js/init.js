beec3a47ec3897830098d3afbc7bce98a9865af4
Monis Qadri <monis0395@gmail.com>
2016-06-17 05:18:48 +0530

    DJS First commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+        $('.slider').slider({ full_width: true });
+        $('.slider').slider({ height: 700 });
+        $(".dropdown-button").dropdown();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
