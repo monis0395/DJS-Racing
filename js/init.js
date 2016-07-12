75ddc3ab22a79e791f346ee05fe9438597e8f3d1
Monis Qadri <monis0395@gmail.com>
2016-07-12 22:02:46 +0530

    Add image to teams page and made some small changes on sponsors page and parallax is working on about us page

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
         $('.slider').slider({ full_width: true });
         $('.slider').slider({ height: 700 });
         $(".dropdown-button").dropdown();
+        $('.parallax').parallax();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
