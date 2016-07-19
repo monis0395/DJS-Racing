3736743f050c8bda12bcc59d4f08db55b8d1decb
Monis Qadri <monis0395@gmail.com>
2016-07-20 00:25:41 +0530

    Added cars page. Remove Orbitron font Added Owl carousel Minor changes in all other files

--- js/init.js
+++ js/init.js
@@ -2,10 +2,23 @@
     $(function() {
 
         $('.button-collapse').sideNav();
-        $('.slider').slider({ full_width: true });
-        $('.slider').slider({ height: 700 });
         $(".dropdown-button").dropdown();
         $('.parallax').parallax();
+        $('.slider').slider({
+            full_width: true
+        });
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            lazyLoad: true,
+            loop: true,
+            margin: 10,
+            autoHeight: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            autoplaySpeed: 2000
+        });
+
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
